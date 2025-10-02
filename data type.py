bf7d6f9e0e09255b2c58cdf5e86629587afbef85
almegastro <sharonamondi0213@gmail.com>
2025-10-02 19:13:59 +0300

    python

---
+++ data type.py
@@ -0,0 +1,21 @@
+name = "ethan"
+age = 10
+is_student = True
+weight = 38.5
+
+print("name is", name)
+print("data type of name is", type(name))
+print("age is", age)
+print("data type of age is", type(age))
+print("is_student is", is_student)
+print("data type of is_student is", type(is_student))
+print("weight is", weight)
+print("data type of weight is", type(weight))
+
+print("\n after type casting....")
+age = str(age)
+print("age is", age)
+print("data type of age is", type(age))
+weight = int(weight)
+print("weight is", weight)
+print("data type of weight is", type(weight))
---
+++ operator.py
@@ -0,0 +1,22 @@
+num1 = 45
+num2 = 8
+
+
+print("number 1 is", num1)
+print("number 2 is", num2)
+print("the sum is", num1 + num2)
+print("the difference is", num1 - num2)
+print("the product is", num1 * num2)
+print("the quotient is", num1 / num2)
+print("the floor division is", num1 // num2)
+print("the remainder is", num1 % num2)
+print("the exponent is", num1 ** num2)
+print("square root of num1 is", num1 ** 0.5)
+
+print("equality", num1 == num2)
+print("inequality", num1 != num2)
+print("greater than", num1 > num2)
+print("less than", num1 < num2)
+
+result = (num1 + num2) * (num1 - num2)
+print("the result is", result)
