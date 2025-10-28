7d804b0053d05529475d1e16a4a69be074eb70d4
almegastro <sharonamondi0213@gmail.com>
2025-10-28 18:44:59 +0300

    number loops

---
+++ loops/natural number sum.py
@@ -0,0 +1,7 @@
+num = 1
+sum = 0
+while num <= 100:
+    sum += num
+    num += 1
+
+print("The sum of natural numbers from 1 to 100 is:", sum)
---
+++ loops/table of 35.py
@@ -0,0 +1,5 @@
+num = 67
+print(f"Table of {num}:")
+for i in range(1, 13):
+    mul = num * i
+    print('35 x %d = %d' % (i, mul))
